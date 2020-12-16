a422fda71722cfc9170ac4b66dbdb89423d52827
范轶洁 <fanyjb@digitalchina.com>
2020-12-16 09:44:08 +0800

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { DcComponentsSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    DcComponentsSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <dc-components-sample />
+  </div>
+</template>
