66b639bd31ceb072939b58ef74dab5f70ca4e9eb
范轶洁 <fanyjb@digitalchina.com>
2020-12-20 22:08:58 +0800

    antd component

---
+++ src/lib-components/dc-another-component.vue
@@ -0,0 +1,16 @@
+<template>
+  <alert message="Success Text" type="success" />
+</template>
+
+<script>
+import Alert from "ant-design-vue/lib/alert";
+import "ant-design-vue/dist/antd.css";
+
+export default {
+  components: {
+    Alert,
+  },
+};
+</script>
+
+<style></style>
